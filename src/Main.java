461f930c526fdf707eb88b8e56c3f4a393d256dd
yekaterinagordeyeva456 <yges1979@gmail.com>
2023-02-09 01:52:32 +0600

    Task 1 saving

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        task1();
     }
+    public static void task1() {
+        System.out.println("Задача 1:");
+        int apple = 33000;
+        System.out.println("Значение переменной apple c типом int равно " + apple);
+        byte white = 5;
+        System.out.println("Значение переменной white c типом byte равно " + white);
+        long red = 3456L;
+        System.out.println("Значение переменной red c типом long равно " + red);
+        short blue = 234;
+        System.out.println("Значение переменной blue c типом short равно " + blue);
+        float color = 1.45675f;
+        System.out.println("Значение переменной color c типом float равно " + color);
+        double gray = 1.56;
+        System.out.println("Значение переменной gray c типом double равно " + gray);
+
+    }
+
 }
